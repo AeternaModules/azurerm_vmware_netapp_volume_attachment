141e03dd983d1c5674fe4d57ecdba25378126f05
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:16:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "vmware_netapp_volume_attachments" {
   description = "All vmware_netapp_volume_attachment resources"
   value       = azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments
 }
+output "vmware_netapp_volume_attachments_name" {
+  description = "List of name values across all vmware_netapp_volume_attachments"
+  value       = [for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : v.name]
+}
+output "vmware_netapp_volume_attachments_netapp_volume_id" {
+  description = "List of netapp_volume_id values across all vmware_netapp_volume_attachments"
+  value       = [for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : v.netapp_volume_id]
+}
+output "vmware_netapp_volume_attachments_vmware_cluster_id" {
+  description = "List of vmware_cluster_id values across all vmware_netapp_volume_attachments"
+  value       = [for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : v.vmware_cluster_id]
+}
 
