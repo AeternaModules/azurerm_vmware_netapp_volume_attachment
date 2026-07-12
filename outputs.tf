4d1d7a36269a34ba1737395ba4eef7e20ee30719
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "vmware_netapp_volume_attachments" {
-  description = "All vmware_netapp_volume_attachment resources"
-  value       = azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments
-}
 output "vmware_netapp_volume_attachments_name" {
-  description = "List of name values across all vmware_netapp_volume_attachments"
-  value       = [for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : v.name]
+  description = "Map of name values across all vmware_netapp_volume_attachments, keyed the same as var.vmware_netapp_volume_attachments"
+  value       = { for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : k => v.name }
 }
 output "vmware_netapp_volume_attachments_netapp_volume_id" {
-  description = "List of netapp_volume_id values across all vmware_netapp_volume_attachments"
-  value       = [for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : v.netapp_volume_id]
+  description = "Map of netapp_volume_id values across all vmware_netapp_volume_attachments, keyed the same as var.vmware_netapp_volume_attachments"
+  value       = { for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : k => v.netapp_volume_id }
 }
 output "vmware_netapp_volume_attachments_vmware_cluster_id" {
-  description = "List of vmware_cluster_id values across all vmware_netapp_volume_attachments"
-  value       = [for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : v.vmware_cluster_id]
+  description = "Map of vmware_cluster_id values across all vmware_netapp_volume_attachments, keyed the same as var.vmware_netapp_volume_attachments"
+  value       = { for k, v in azurerm_vmware_netapp_volume_attachment.vmware_netapp_volume_attachments : k => v.vmware_cluster_id }
 }
 
